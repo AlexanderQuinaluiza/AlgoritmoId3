218cc665d343e6f014baa59711e5aa4812e2a797
IVAN <IVAN@IVANQ>
2015-06-06 00:02:39 -0500

    configuracion 1

--- miproyectoId3/src/miproyectoid3/MiproyectoId3.java
+++ miproyectoId3/src/miproyectoid3/MiproyectoId3.java
@@ -15,7 +15,20 @@ public class MiproyectoId3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+      AlgoritmoID3 id =new AlgoritmoID3();
+		Object datos[][]=new Object[][]{{"c","Joe","No","Sally","Alto"},{ "c","Samantha","No","Thomas","Bajo"},{ "a","Jim","Si","Patrick","Bajo"},
+				{ "c","Joe","No","Thomas","Bajo"},{ "b","Jim","No","Patrick","Alto"},{ "b","Jim","Si","Thomas","Bajo"}
+				,{ "a","Joe","No","Patrick","Alto"},{ "b","Samantha","Si","Patrick","Bajo"}};
+		
+		id.id3(datos,0);
+		//System.out.println(id.logn(0, 2));
+		String v[][]=id.formarReglas();
+		
+		for (String[] strings : v) {
+			for (String string : strings) {
+				System.out.print(string+" ");
+			}System.out.println();  // TODO code application logic here
+    }  // TODO code application logic here
     }
     
 }
