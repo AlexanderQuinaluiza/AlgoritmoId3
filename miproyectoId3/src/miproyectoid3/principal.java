23a8e51aea3bb6425ec26052ed6c440fbf6dab2a
crisselli <jimenezcristina28@gmail.com>
2015-06-08 19:28:42 -0500

    Update principal.java

--- miproyectoId3/src/miproyectoid3/principal.java
+++ miproyectoId3/src/miproyectoid3/principal.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package id3aq;
+package miproyectoid3;
 
 import java.awt.event.KeyEvent;
 import javax.swing.JComboBox;
